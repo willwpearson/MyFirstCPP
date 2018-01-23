2523b8d968cf2246649cfe55d7e60c1c8da50db5
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-23 12:38:07 -0700

    Created a new folder with a new main method. Code successfully does nothing.

---
+++ MyFirstCPP/Controller/Runner.cpp
@@ -0,0 +1,13 @@
+//
+//  Runner.cpp
+//  MyFirstCPP
+//
+//  Created by Pearson, William on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+int main()
+{
+    
+    return 0;
+}
--- MyFirstCPP/main.cpp
+++ MyFirstCPP/main.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-int main()
+int oldMain()
 {
     // insert code here...
     cout << "Hello, World!" << endl;
