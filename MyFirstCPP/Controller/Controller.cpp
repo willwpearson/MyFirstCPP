ca9cf1d4cb40d05c7c2007552704968ee9321636
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-23 12:46:00 -0700

    Added the class file for the controller. Made the class header for the controller.

---
+++ MyFirstCPP/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MyFirstCPP
+//
+//  Created by Pearson, William on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MyFirstCPP/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  MyFirstCPP
+//
+//  Created by Pearson, William on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
