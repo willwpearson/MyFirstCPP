991ea0865d9b2ecaf9d7c2297a2cf0cd5a5baa2f
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-23 12:30:30 -0700

    Created a new practice project for CPP

---
+++ MyFirstCPP/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  MyFirstCPP
+//
+//  Created by Pearson, William on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    string input;
+    getline(cin, input);
+    cout << "You said: " << input << endl;
+    return 0;
+}
