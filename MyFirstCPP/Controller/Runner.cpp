2483be1c983299edd2cc93c83e9127b914f396fa
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-23 13:50:20 -0700

    Added objects to mess around with CPP.

--- MyFirstCPP/Controller/Runner.cpp
+++ MyFirstCPP/Controller/Runner.cpp
@@ -6,8 +6,15 @@
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
+#include "Controller.hpp"
+
 int main()
 {
+    Controller app;
+    app.start();
     
+    Controller * appPointer = new Controller();
+    appPointer->start();
+    (*appPointer).start();
     return 0;
 }
